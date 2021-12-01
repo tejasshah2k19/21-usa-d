e5521efe631fd088421b77aa1b3231b38dc13207
Tejas <tejas14all@gmail.com>
2021-12-01 08:21:34 +0530

    session-1

---
+++ src/com/controller/SessionLoginServet.java
@@ -0,0 +1,28 @@
+package com.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/SessionLoginServet")
+public class SessionLoginServet extends HttpServlet {
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+
+		// authenticate -- true
+
+		HttpSession session = request.getSession();
+		session.setAttribute("email", email);//
+
+		response.sendRedirect("SessionHome.jsp");
+	}
+}
